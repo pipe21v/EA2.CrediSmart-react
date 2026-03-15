48b68386b8af94cbbb5a903fadb1138f79cd9924
pipe21v <luis.ladino@est.iudigital.edu.co>
2026-03-15 09:30:09 -0500

    Commit1: configuracion inicial React + Vite y estructura de carpetas base

---
+++ README.md
@@ -0,0 +1,9 @@
+Credismart segunda parte proyecto Creditsmart, utilizando react+vite , nodeJs
+
+Nombre: Luis Felipe Ladino Monsalve
+
+Descripción del Proyecto
+Desarrollo de Aplicación Web Dinámica con React donde ahora los datos se manejan mediante estado y los componentes son reutilizables.
+
+Estructura de Archivos
+
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+      </section>
+
+  
+    </>
+  )
+}
+
+export default App
